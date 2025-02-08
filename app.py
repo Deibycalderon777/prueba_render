c89e175d4960340f6d447e14bd4573d303fe5f6e
Deiby Enrique Calderon <164361682+Deibycalderon777@users.noreply.github.com>
2025-02-08 13:21:57 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 from flask import Flask
-
 from flask import render_template
 from flask import request
 from flask import jsonify, make_response
-
 import mysql.connector
 
 con = mysql.connector.connect(
